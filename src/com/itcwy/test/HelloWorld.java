2794a1854da48c1c850c0da52a9f1223f92fd839
chi <460611681@qq.com>
2019-05-16 01:38:36 +0800

    修改第一次

--- src/com/itcwy/test/HelloWorld.java
+++ src/com/itcwy/test/HelloWorld.java
@@ -4,5 +4,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("HelloWorld!");
         System.out.println("HelloWorld2!");
+        System.out.println("HelloWorld5!");
+        System.out.println("HelloWorld6!");
     }
 }
