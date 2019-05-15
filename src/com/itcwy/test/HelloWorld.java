804d68e8ddff4f8ea7bbae9150ea400e90426b3f
chi <460611681@qq.com>
2019-05-16 01:21:55 +0800

    修改第一次

--- src/com/itcwy/test/HelloWorld.java
+++ src/com/itcwy/test/HelloWorld.java
@@ -3,5 +3,6 @@ package com.itcwy.test;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("HelloWorld!");
+        System.out.println("HelloWorld2!");
     }
 }
